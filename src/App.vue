838b354f03abb482bac1e540e35765c92fa26dff
Nadezhda Boldyreva <boldyrevanadezhda@gmail.com>
2021-06-28 17:03:40 +0700

    Added the multilang plugin

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header/>
+
     <router-view/>
+
+    <Footer/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/common/header'
+import Footer from '@/components/common/footer'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>

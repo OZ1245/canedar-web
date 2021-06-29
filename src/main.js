58b02b4b3072cbebee65944455a28e46f9e9202c
Nadezhda Boldyreva <boldyrevanadezhda@gmail.com>
2021-06-29 13:57:30 +0700

    Installed moment.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store'
 import apolloProvider from './graphql/apollo'
 import './ml'
+import moment from 'moment'
 
+Vue.prototype.$moment = moment
 Vue.config.productionTip = false
 
 new Vue({
